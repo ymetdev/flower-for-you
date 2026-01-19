8cfaadef5cd841d65286c08d95ff5ec283398416
ymetdev <tembadboi@gmail.com>
2026-01-19 14:03:17 +0700

    Fix vite.config

--- Frontend/flower-user/vite.config.js
+++ Frontend/flower-user/vite.config.js
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [tailwindcss()],
+  server: {
+    proxy: {
+      // เมื่อคุณเรียก fetch('/api/...') ใน localhost
+      // Vite จะส่งคำขอไปที่ Server ปลายทางให้โดยอัตโนมัติ
+      "/api": {
+        target: "http://72.62.243.238:5000",
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, ''), // ปลดคอมเมนต์ถ้าต้องการตัดคำว่า /api ออกก่อนส่งไป server
+      },
+    },
+  },
+});
